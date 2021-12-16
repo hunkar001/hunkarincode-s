e18cf96262ecc24b4d031fd88dc7fad415def05e
hunkar001 <95900592+hunkar001@users.noreply.github.com>
2021-12-16 21:26:15 +0300

    g

---
+++ isimsiz.py
@@ -0,0 +1,18 @@
+import math
+
+def kokvarmi(sayi):
+    """
+    >>> kokvarmi(16)
+    True
+    >>> kokvarmi(17)
+    False
+    >>> kokvarmi(36)
+    True
+
+    """
+    return math.sqrt(sayi)%1 == 0
+
+if __name__ == "__main__" :
+
+    import doctest
+    doctest.testmod(verbose=True)
